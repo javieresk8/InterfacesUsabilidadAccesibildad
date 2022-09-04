e00b8660665880dd7a617cdf17c547f0f58c6854
Andres Vargas <stalin.vargas@epn.edu.ec>
2022-09-04 12:59:15 -0500

    /Update footer_finalizado

--- Interfaces/proyecto/src/app/Componentes/footer/footer/footer.component.ts
+++ Interfaces/proyecto/src/app/Componentes/footer/footer/footer.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  twiter = 'https://cdn-icons-png.flaticon.com/24/733/733579.png'
 
+  facebook = 'https://cdn-icons-png.flaticon.com/24/5968/5968764.png'
+  youtube = 'https://cdn-icons-png.flaticon.com/24/187/187210.png'
+  instagram = 'https://cdn-icons-png.flaticon.com/24/3955/3955024.png'
   constructor() { }
 
   ngOnInit(): void {
