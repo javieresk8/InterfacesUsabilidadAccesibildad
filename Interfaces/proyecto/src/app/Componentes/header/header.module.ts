dc21a05e708502c0797dfa37aacfaa1cf8a2a6b2
Diego Márquez <57378696+oddyEC@users.noreply.github.com>
2022-09-06 12:20:16 -0500

    navegacion

--- Interfaces/proyecto/src/app/Componentes/header/header.module.ts
+++ Interfaces/proyecto/src/app/Componentes/header/header.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
-
-
-
+import { MatSidenavModule } from '@angular/material/sidenav'
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import { RouterModule, Routes } from '@angular/router';
 @NgModule({
     declarations: [
         HeaderComponent,
@@ -12,10 +13,12 @@ import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
     ],
     exports: [
         HeaderComponent,
-        BreadcrumbComponent
+        BreadcrumbComponent,
+        [MatMenuModule], [MatIconModule], [RouterModule]
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        [MatMenuModule], [MatIconModule], [RouterModule]
     ]
 })
 export class HeaderModule { }
