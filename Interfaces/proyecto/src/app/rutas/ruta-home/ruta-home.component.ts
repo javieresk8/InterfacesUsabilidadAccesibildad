7cedc19aaa33f25c57055821b47e71ceb991ed47
Diego Márquez <57378696+oddyEC@users.noreply.github.com>
2022-09-07 15:28:01 -0500

    carousel fix

--- Interfaces/proyecto/src/app/rutas/ruta-home/ruta-home.component.ts
+++ Interfaces/proyecto/src/app/rutas/ruta-home/ruta-home.component.ts
@@ -10,9 +10,9 @@ export class RutaHomeComponent implements OnInit {
   imagen_destacada1="https://www.trabajo.gob.ec/wp-content/uploads/2022/08/1-3.jpg"
   imagen_destacada2 = 'https://www.trabajo.gob.ec/wp-content/uploads/2022/08/WhatsApp-Image-2022-08-24-at-5.08.32-PM-e1661379119616.jpeg'
   images = [
-    {title: 'First Slide', short: 'First Slide Short', src: "https://www.trabajo.gob.ec/wp-content/uploads/2022/09/Banner%20web.png?x42051"},
-    {title: 'Second Slide', short: 'Second Slide Short', src: "https://picsum.photos/id/1011/900/500"},
-    {title: 'Third Slide', short: 'Third Slide Short', src: "https://picsum.photos/id/984/900/500"}
+    {src: "https://www.trabajo.gob.ec/wp-content/uploads/2022/09/Banner%20web.png?x42051"},
+    {src: "https://picsum.photos/id/1011/900/500"},
+    {src: "https://picsum.photos/id/984/900/500"}
   ];
   constructor(config: NgbCarouselConfig) { 
     config.interval = 2000;
