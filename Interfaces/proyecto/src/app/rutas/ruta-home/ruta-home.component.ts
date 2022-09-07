bc47c5806d1044149402e753db685457fe8fdb83
Diego Márquez <57378696+oddyEC@users.noreply.github.com>
2022-09-06 22:18:50 -0500

    fixed carusel

--- Interfaces/proyecto/src/app/rutas/ruta-home/ruta-home.component.ts
+++ Interfaces/proyecto/src/app/rutas/ruta-home/ruta-home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ruta-home.component.scss']
 })
 export class RutaHomeComponent implements OnInit {
-
+  imagen_destacada1="https://www.trabajo.gob.ec/wp-content/uploads/2022/08/1-3.jpg"
+  imagen_destacada2 = 'https://www.trabajo.gob.ec/wp-content/uploads/2022/08/WhatsApp-Image-2022-08-24-at-5.08.32-PM-e1661379119616.jpeg'
   constructor() { }
 
   ngOnInit(): void {
